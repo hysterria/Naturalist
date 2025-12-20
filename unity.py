f16fab394eaa2c3ba571b50dc1e4b6200689a4b1
кошька <prirychaudrakonov@gmail.com>
2025-12-20 18:57:57 +0300

    fix dataset

--- unity.py
+++ unity.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 csv_files = [
     "observations-660868.csv",
     "observations-660877.csv",
-    "observations-660879.csv",
+    "observations-660903.csv",
     "observations-660882.csv",
     "observations-660885.csv",
     "observations-660887.csv"
@@ -22,26 +22,23 @@ for file in csv_files:
 full_df = pd.concat(dfs, ignore_index=True)
 
 full_df.to_csv("full_dataset.csv", index=False)
-print("Объединённый CSV сохранён как full_dataset.csv")
 
 
 base_folder = Path("data/images")
 base_folder.mkdir(parents=True, exist_ok=True)
 
-print("Начинаем скачивание изображений...")
 for _, row in tqdm(full_df.iterrows(), total=len(full_df)):
     url = row['image_url']
     label = row['label']
 
     if pd.isna(url) or url.strip() == "":
-        continue  # пропускаем пустые ссылки
+        continue 
 
     folder = base_folder / label
     folder.mkdir(parents=True, exist_ok=True)
 
     img_path = folder / f"{row['id']}.jpg"
 
-    # Скачиваем изображение
     try:
         img_data = requests.get(url, timeout=10).content
         with open(img_path, 'wb') as f:
@@ -49,4 +46,4 @@ for _, row in tqdm(full_df.iterrows(), total=len(full_df)):
     except Exception as e:
         print(f"Ошибка при скачивании {url}: {e}")
 
-print("Скачивание завершено!")
+print("Скачивание завершено")
